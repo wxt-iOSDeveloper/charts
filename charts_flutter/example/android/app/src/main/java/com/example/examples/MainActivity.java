73520cbe5f2bf68f0f868170eaf5bab08241251e
gray <493628242@qq.com>
2022-10-23 18:29:18 +0800

    升级版本 可以直接运行example combo_chart.dart 构造方法中添加 userManagedState字段

--- charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
+++ charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
@@ -1,17 +1,10 @@
 package com.example.examples;
 
-import android.os.Bundle;
 
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 /**
  * FlutterActivity
  */
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
 }
